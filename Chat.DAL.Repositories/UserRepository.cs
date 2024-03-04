6ec4a495e01ba0e0793e4d82c5a2e8aa9380a826
AleksandrDev <myshalow_aleksandar@mail.ru>
2024-03-04 21:27:43 +0300

    Add user creation

--- Chat.DAL.Repositories/UserRepository.cs
+++ Chat.DAL.Repositories/UserRepository.cs
@@ -1,9 +1,21 @@
-﻿using Chat.DAL.Repositories.Contracts;
+﻿using Chat.Common.Entities;
+using Chat.DAL.Repositories.Contracts;
 
 namespace Chat.DAL.Repositories
 {
     public class UserRepository : IUserRepository
     {
+        private readonly ChatContext _chatContext;
+
+        public UserRepository(ChatContext chatContext)
+        {
+            _chatContext = chatContext;
+        }
+
+        public async Task Create(User user)
+        {
+            await _chatContext.Users.AddAsync(user);
+        }
 
     }
 }
